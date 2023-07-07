a83e3f68457edb7e915758ac718b296be941c939
Omar Torres <omartorres980.com>
2023-07-06 22:33:58 -0700

    added background image

--- ufo-sightings/src/pages/About.js
+++ ufo-sightings/src/pages/About.js
@@ -13,30 +13,29 @@ const About = () => {
   }, []);
 
   return (
-    <div className="about">
-      <div className="animation-container">
-        <div className="moon-container">
-          
-          <lottie-player
-            src="https://assets1.lottiefiles.com/packages/lf20_Tkgg8F37jX.json"
-            background="transparent"
-            speed="1"
-            style={{ width: '300px', height: '300px' }}
-            loop
-            autoplay
-          ></lottie-player>
+    <div className='background-img'>
+      <div className="about">
+        <div className="animation-container">
+          <div className="moon-container">
+            <lottie-player
+              src="https://assets1.lottiefiles.com/packages/lf20_Tkgg8F37jX.json"
+              background="transparent"
+              speed="1"
+              style={{ width: '300px', height: '300px' }}
+              loop
+              autoplay
+            ></lottie-player>
+          </div>
+          <div className="phrase-container">
+            <div className="phrase phrase-1">I am an alien enthusiast just like you</div>
+            <div className="phrase phrase-2">I would take a souvenir from a UFO</div>
+            <div className="phrase phrase-3">Reptilian people are among us</div>
+            <div className="phrase phrase-4">Count to ten if you're not an alien</div>
+            <div className="phrase phrase-5">Men in Black was my first likeable alien movie</div>
+          </div>
         </div>
-
-        <div className="letter-container">
-        <div className="letter letter-1">I am an alien enthusiast just like you</div>
-        <div className="letter letter-2">I would take a souvenir from a UFO</div>
-        <div className="letter letter-3">Reptilian people are among us</div>
-        <div className="letter letter-4">Count to ten if you're not an alien</div>
-        <div className="letter letter-5">Men in Black was my first likeable alien movie</div>
-        </div>
-
+        <h1 className="rotating-text">About Me</h1>
       </div>
-      <h1 className="rotating-text">About Me</h1>
     </div>
   );
 };
