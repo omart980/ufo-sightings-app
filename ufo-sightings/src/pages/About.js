fc8796cd5f9892009859855faa96c3c7d0357d93
Omar Torres <omartorres980.com>
2023-07-06 21:55:24 -0700

    added floating text in About page

--- ufo-sightings/src/pages/About.js
+++ ufo-sightings/src/pages/About.js
@@ -7,7 +7,6 @@ const About = () => {
     script.src = 'https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js';
     script.async = true;
     document.body.appendChild(script);
-
     return () => {
       document.body.removeChild(script);
     };
@@ -17,6 +16,7 @@ const About = () => {
     <div className="about">
       <div className="animation-container">
         <div className="moon-container">
+          
           <lottie-player
             src="https://assets1.lottiefiles.com/packages/lf20_Tkgg8F37jX.json"
             background="transparent"
@@ -26,6 +26,15 @@ const About = () => {
             autoplay
           ></lottie-player>
         </div>
+
+        <div className="letter-container">
+        <div className="letter letter-1">I am an alien enthusiast just like you</div>
+        <div className="letter letter-2">I would take a souvenir from a UFO</div>
+        <div className="letter letter-3">Reptilian people are among us</div>
+        <div className="letter letter-4">Count to ten if you're not an alien</div>
+        <div className="letter letter-5">Men in Black was my first likeable alien movie</div>
+        </div>
+
       </div>
       <h1 className="rotating-text">About Me</h1>
     </div>
