ce20ceaf6dace13d96b72472f69d09464377a971
Omar Torres <omartorres980.com>
2023-07-25 13:49:08 -0700

    added adaptive screen size for About page

--- ufo-sightings/src/pages/About.js
+++ ufo-sightings/src/pages/About.js
@@ -13,7 +13,7 @@ const About = () => {
   }, []);
 
   return (
-    <main className="container background-image d-flex justify-content-center align-items-center vh-100">
+    <main className="container d-flex justify-content-center align-items-center vh-100">
       <section>
         <section className="row">
           <section className="col rotating-text">
@@ -21,14 +21,14 @@ const About = () => {
           </section>
         </section>
 
-        <section className='row row-cols-2 text-warning'>
+        <section className='row description row-cols-2 text-warning'>
           <section className="col p-5">I am an alien enthusiast just like you</section>
           <section className="col p-5">I would take a souvenir from a UFO</section>
           <section className="col p-5">Reptilian people are among us</section>
           <section className="col p-5">Count to ten if you're not an alien</section>
         </section>
 
-        <section className="d-flex justify-content-center align-items-center moon-container"> {/* Center the animation */}
+        <section className="d-flex moon-container justify-content-center align-items-center">
           <lottie-player
             src="https://assets1.lottiefiles.com/packages/lf20_Tkgg8F37jX.json"
             background="transparent"
