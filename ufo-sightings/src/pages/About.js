a079a3040eb5a2e5ca6aa85a7a60e036b3612e4c
Omar Torres <omartorres980.com>
2023-07-27 14:32:32 -0700

    added background image for About page

--- ufo-sightings/src/pages/About.js
+++ ufo-sightings/src/pages/About.js
@@ -13,33 +13,35 @@ const About = () => {
   }, []);
 
   return (
-    <main className="container d-flex justify-content-center align-items-center vh-100">
-      <section>
-        <section className="row">
-          <section className="col rotating-text">
-            <h1 className='text-white'>About Me</h1>
+    <div className='background-image'>
+      <main className="container d-flex justify-content-center align-items-center vh-100">
+        <section>
+          <section className="row">
+            <section className="col rotating-text">
+              <h1 className='text-white'>About Me</h1>
+            </section>
+          </section>
+
+          <section className='row description row-cols-2 text-warning'>
+            <section className="col p-5">I am an alien enthusiast just like you</section>
+            <section className="col p-5">I once saw flashing lights in the sky</section>
+            <section className="col p-5">Reptilian people are among us</section>
+            <section className="col p-5">Count to ten if you're not an alien</section>
+          </section>
+
+          <section className="d-flex moon-container justify-content-center align-items-center">
+            <lottie-player
+              src="https://assets1.lottiefiles.com/packages/lf20_Tkgg8F37jX.json"
+              background="transparent"
+              speed="1"
+              loop
+              autoplay
+            >
+            </lottie-player>
           </section>
         </section>
-
-        <section className='row description row-cols-2 text-warning'>
-          <section className="col p-5">I am an alien enthusiast just like you</section>
-          <section className="col p-5">I would take a souvenir from a UFO</section>
-          <section className="col p-5">Reptilian people are among us</section>
-          <section className="col p-5">Count to ten if you're not an alien</section>
-        </section>
-
-        <section className="d-flex moon-container justify-content-center align-items-center">
-          <lottie-player
-            src="https://assets1.lottiefiles.com/packages/lf20_Tkgg8F37jX.json"
-            background="transparent"
-            speed="1"
-            loop
-            autoplay
-          >
-          </lottie-player>
-        </section>
-      </section>
-    </main>
+      </main>
+    </div>
   );
 };
 
