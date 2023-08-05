522cad4a278d4c3e278e77f5333d506138fb3f7a
Omar Torres <omartorres980.com>
2023-08-05 00:57:14 -0700

    Updates

---
+++ precache-manifest.faf2afee8d1a7b0e89caaea1e1782392.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "29be1b11a780ba1e6762",
+    "url": "/ufo-sightings-app/static/css/main.a7d6141c.chunk.css"
+  },
+  {
+    "revision": "29be1b11a780ba1e6762",
+    "url": "/ufo-sightings-app/static/js/main.81da51b9.chunk.js"
+  },
+  {
+    "revision": "3a83cf4e8be7364c29cd",
+    "url": "/ufo-sightings-app/static/js/runtime~main.7475c605.js"
+  },
+  {
+    "revision": "e0ae43b1f6fe210004e7",
+    "url": "/ufo-sightings-app/static/css/2.41229961.chunk.css"
+  },
+  {
+    "revision": "e0ae43b1f6fe210004e7",
+    "url": "/ufo-sightings-app/static/js/2.ee4769be.chunk.js"
+  },
+  {
+    "revision": "fe3ca953df6f7baf489f",
+    "url": "/ufo-sightings-app/static/js/3.43aeda61.chunk.js"
+  },
+  {
+    "revision": "9a1af1a70bc4b12884be0805a77c1932",
+    "url": "/ufo-sightings-app/static/media/alien_logo.9a1af1a7.jpg"
+  },
+  {
+    "revision": "f18819e36c78cfbcd68e15d3e52f8efb",
+    "url": "/ufo-sightings-app/static/media/Aliencow.f18819e3.jpeg"
+  },
+  {
+    "revision": "af82b631608e3af17a8875597e69708f",
+    "url": "/ufo-sightings-app/static/media/meme2021.af82b631.jpg"
+  },
+  {
+    "revision": "c4d345c0efd9aa226bd5cdbcc2b65a98",
+    "url": "/ufo-sightings-app/static/media/alienvspredator.c4d345c0.jpg"
+  },
+  {
+    "revision": "305c9158d9c9aeda03f847bfc995a28a",
+    "url": "/ufo-sightings-app/static/media/americandad.305c9158.jpg"
+  },
+  {
+    "revision": "2b034a263c31ccb502d62b83f1c17acd",
+    "url": "/ufo-sightings-app/static/media/baby.2b034a26.jpg"
+  },
+  {
+    "revision": "db4b3c5faf4f2ed994bbdbcbc3dfd38f",
+    "url": "/ufo-sightings-app/static/media/couch.db4b3c5f.jpg"
+  },
+  {
+    "revision": "90b46e64e54b57022ea693db9241d297",
+    "url": "/ufo-sightings-app/static/media/crudo.90b46e64.jpg"
+  },
+  {
+    "revision": "a82507efe30c32002d3ebef1d0e3d59d",
+    "url": "/ufo-sightings-app/static/media/drugs.a82507ef.jpg"
+  },
+  {
+    "revision": "cdcf3cc7b877c3c95ac2557ef2bd0e80",
+    "url": "/ufo-sightings-app/static/media/everywhere.cdcf3cc7.jpg"
+  },
+  {
+    "revision": "acd022b301ba4e5f85f70eaf737ebdcf",
+    "url": "/ufo-sightings-app/static/media/hey.acd022b3.jpg"
+  },
+  {
+    "revision": "ab9b7655e5f5b5b304e8de34cfa02611",
+    "url": "/ufo-sightings-app/static/media/humans.ab9b7655.jpg"
+  },
+  {
+    "revision": "b13657223edf2e5b4cb7b8b790ad8771",
+    "url": "/ufo-sightings-app/static/media/idioma.b1365722.jpg"
+  },
+  {
+    "revision": "bd0124186a777adc02fd3b3975a48142",
+    "url": "/ufo-sightings-app/static/media/job.bd012418.jpg"
+  },
+  {
+    "revision": "61f49ad54cbd66b0795a63e03ca67faf",
+    "url": "/ufo-sightings-app/static/media/meeting.61f49ad5.jpg"
+  },
+  {
+    "revision": "16423e9575167adb0d0e84c9acb692b8",
+    "url": "/ufo-sightings-app/static/media/millionaire.16423e95.jpg"
+  },
+  {
+    "revision": "1e5b654f658df0c6b744991ebcff669c",
+    "url": "/ufo-sightings-app/static/media/money.1e5b654f.jpg"
+  },
+  {
+    "revision": "bfd667ab1f4ec8bda0f0ba0b97592630",
+    "url": "/ufo-sightings-app/static/media/otravez.bfd667ab.jpg"
+  },
+  {
+    "revision": "203ab8ad071872fd35ef81d3a8eac171",
+    "url": "/ufo-sightings-app/static/media/overhead.203ab8ad.jpeg"
+  },
+  {
+    "revision": "441d1445868f05a95d66e8868e1e8233",
+    "url": "/ufo-sightings-app/static/media/selfparking.441d1445.jpeg"
+  },
+  {
+    "revision": "a85ee6379bb574ae5e6915175b282e13",
+    "url": "/ufo-sightings-app/static/media/simpsons.a85ee637.jpg"
+  },
+  {
+    "revision": "88df6e3fad8cc022c3c2fc77f090b2eb",
+    "url": "/ufo-sightings-app/static/media/toiletpaper.88df6e3f.jpg"
+  },
+  {
+    "revision": "a8276aaa73a76351e2bdbb8508134914",
+    "url": "/ufo-sightings-app/static/media/toystory.a8276aaa.jpeg"
+  },
+  {
+    "revision": "339879be3d1cbdac349a05b374cdf2b9",
+    "url": "/ufo-sightings-app/static/media/truth.339879be.jpeg"
+  },
+  {
+    "revision": "866e82c98d616a8af2fa495aece883f2",
+    "url": "/ufo-sightings-app/static/media/about_space.866e82c9.jpeg"
+  },
+  {
+    "revision": "e6e2ce1dd2bb531c498b7e7b498f4080",
+    "url": "/ufo-sightings-app/static/media/night-sky-stars.e6e2ce1d.jpg"
+  },
+  {
+    "revision": "b0a1fc609ea08d3c630ecf99ac20f5ad",
+    "url": "/ufo-sightings-app/static/media/media_earthat.b0a1fc60.jpg"
+  },
+  {
+    "revision": "5bdd521a7f526257b942372c8c890f84",
+    "url": "/ufo-sightings-app/index.html"
+  }
+];
